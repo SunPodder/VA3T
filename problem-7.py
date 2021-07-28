1aa3a14fe528b12b7b8795c147f2b57046f11cf5
Sun <73415680+SunPodder@users.noreply.github.com>
2021-07-28 20:00:51 +0600

    Added exit functionality

--- problem-7.py
+++ problem-7.py
@@ -1,12 +1,19 @@
 def run(input):
-	
-	if "hi" == input:
+	name = "Bot"
+	if "hi" == input or "hello" == input:
 		print("hi")
 	elif "how are you" == input:
 		print("Fine, you?")
 	elif "what is your name" == input or "who are you" == input:
-		print("I am Bot")
+		print("I am", name)
+	elif "exit" == input:
+		print("exited")
+		return False
+	else:
+		print("Sorry.. I can't understand what are you asking for.")
 	
 while True:
 	cmd = input(">> ").lower()
-	run(cmd)
+	fun = run(cmd)
+	if fun == False:
+		break
